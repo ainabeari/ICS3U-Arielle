68b6bee680ca4e726bdc56862536583cf2cd0d5a
azmania <1ainabeari@hdsb.ca>
2018-07-02 17:45:33 -0400

    Added java files to new repository.

---
+++ ICS3U-Arielle/src/ics3u/arielle/ICS3UArielle.java
@@ -0,0 +1,22 @@
+/*
+ * Arielle
+ * Monday July 2, 2018
+ * Creating new repository from ICS3U. 
+ */
+package ics3u.arielle;
+
+/**
+ *
+ * @author A & J
+ */
+public class ICS3UArielle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // HELLO WORLD
+        System.out.println("Hello World!");
+    }
+    
+}
